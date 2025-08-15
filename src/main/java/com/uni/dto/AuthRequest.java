ff832f4d8d40cfadd723f90016e636d62c5e5271
Yasmine Ahmed <113296463+yasmineahmed11@users.noreply.github.com>
2025-08-15 22:59:01 +0300

    Delete src directory

--- src/main/java/com/uni/dto/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.uni.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthRequest {
-    private String username;
-    private String password;
-}
--- src/main/java/com/uni/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.uni.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
